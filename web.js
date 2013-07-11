67420fe779f3852e81267bc78ceb56d05e18d661
Tibtib Ol <tibtibol01@gmail.com>
2013-07-11 06:54:27 +0000

    Return contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  // response.send('Hello World 2!');
+  var result = fs.readFileSync('index.html').toString();
+  response.send(result);
 });
 
 var port = process.env.PORT || 5000;
